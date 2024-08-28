d208b47acf3ef8af2034347579b6686a69432445
judy <joudydokkmak@gmail.com>
2024-08-28 18:24:25 +0300

    add a user list component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  searchValue= "" ;
+  searchUser()
+  {
+    
+  }
 
 }
